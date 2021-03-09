6ffdad9b3f1358967e2053c8df600db9fd525243
Lirike <lirike.hoxha@gmail.com>
2021-03-09 16:53:15 +0100

    display Owner Record. closes #53

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(1L).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
