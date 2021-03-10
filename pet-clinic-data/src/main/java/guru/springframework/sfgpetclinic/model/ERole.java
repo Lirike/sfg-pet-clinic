5e835d3af1e336fa78afde6db0c2a5f5b527ddb3
Lirike <lirike.hoxha@gmail.com>
2021-03-10 10:40:58 +0100

    Add Spring Security, JWT Authentication

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/ERole.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN,
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.User;
+import guru.springframework.sfgpetclinic.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+    return UserDetailsImpl.build(user);
+  }
+
+}
